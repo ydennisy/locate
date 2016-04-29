4d47c0c1b30cbfec8b94693fc03ec3ce66bc576a
ydennisy <dennis.yurkevich@gmail.com>
2016-04-29 18:33:03 +0100

    update port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var getFiles = require('./routes/getFiles');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
